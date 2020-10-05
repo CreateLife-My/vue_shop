b39611834db09352c4656c1aae70a00701136d05
huang <784256821@qq.com>
2020-10-05 14:39:02 +0800

    first commit

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,9 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import {Button , Form , FormItem ,Input ,Message } from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+// 把message组件挂在在Vue原型中
+Vue.prototype.$message = Message
